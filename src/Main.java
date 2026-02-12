db5b165af4be112abdcc8701f2471ca32d60c6bb
Joseph Hamrick <yclubstep58@gmail.com>
2026-02-12 12:12:14 -0500

    Added the ability to input a name for the program to use in the output

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Name: ");
+        String name = sc.next();
+        System.out.printf("Hello and welcome, " + name);
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hello World! Today is a good day!");
         }
     }
-}

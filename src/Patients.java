612980268d872a2565bcd3f878903b7f274e4c79
Joseph Hamrick <yclubstep58@gmail.com>
2026-02-26 11:47:36 -0500

    Start of Hospital Simulation. If things go very wrong, this is the very start or something, I don't know

--- src/Patients.java
+++ src/Patients.java
@@ -1,9 +1,9 @@
-public class Patients {
+public class Patient {
     private String patient_name;
     public enum Sex{male,female}
     private Sex patient_sex;
     Sex getPatient_sex(){return patient_sex;}
     String getPatient_name(){return patient_name;}
 
-    public Patients(String Name)
+    public Patient(String Name)
 }

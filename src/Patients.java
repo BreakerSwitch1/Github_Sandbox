41205695b187637cd8b7dad64239197c5ae12e48
Joseph Hamrick <yclubstep58@gmail.com>
2026-02-26 12:02:44 -0500

    Started code for patient class and hospital class

--- src/Patients.java
+++
@@ -1,9 +0,0 @@
-public class Patient {
-    private String patient_name;
-    public enum Sex{male,female}
-    private Sex patient_sex;
-    Sex getPatient_sex(){return patient_sex;}
-    String getPatient_name(){return patient_name;}
-
-    public Patient(String Name)
-}

84ecaf1bd596259a7b4027d82fc8ab78466a7334
Joseph Hamrick <yclubstep58@gmail.com>
2026-03-10 11:16:24 -0400

    Started Alert Class and added variables for time that do nothing

--- src/Simulation.java
+++ src/Simulation.java
@@ -4,6 +4,9 @@ public class Simulation {
     }
 
     public void run(){
+        double StartTime = 0.0;
+        double CurrentTime = 0.0;
+        double EndTime = 100.0;
         Hospital hospital1 = new Hospital();
     }
 }

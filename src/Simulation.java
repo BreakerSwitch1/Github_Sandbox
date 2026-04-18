5467e8cb7304e49489bb2cd9b47311d650c3df90
Zoe-Wade <zoe.my.209@gmail.com>
2026-04-17 22:58:13 -0400

    Update Simulation.java

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,55 +1,47 @@
-import java.sql.Time;
-import java.util.Scanner;
+import java.util.Queue;
 import java.util.Random;
-import java.util.random.RandomGenerator;
+import java.util.Scanner;
+import java.io.*;
+public class Hospital {
+    private Patient[] patients;
+    private Nurse[] nurses;
+    Random rng = new Random();
+    private Queue highPriorityQueue;
+    private Queue lowPriorityQueue;
+    private Queue resolvedQueue;
 
-public class Simulation {
-    private Hospital hospital;
-    private int CurrentTime = 0;//Time in Minutes
-    private final int EndTime = 1440;
-    private final int TimeIncrement = 5;
-    int PatientCount = 1;
-    int NurseCount = 6;
-    int getTime(){return CurrentTime;}
-    public Simulation(){
-        //run();
+    public Hospital() {
+        Queue q1 = highPriorityQueue;
+        Queue q2 = lowPriorityQueue;
+        Queue resolved = resolvedQueue;
     }
-    public void create_new_hospital(){
-        Hospital hospital = new Hospital();
 
-    public void setup(){
-        hospital = new Hospital();
-        hospital.hospital_setup(PatientCount,NurseCount);
+    public void hospital_setup(int patient_count, int nurse_count){
+        patients = new Patient[patient_count];
+        for(int i = 0; i<patients.length; i++){
+            patients[i] = Patient.makePatient();
+            patients[i].addDevice(new TemperatureMonitor());
+        }
+
+        nurses = new Nurse[nurse_count];
+        for(int n = 0; n<nurses.length; n++){
+            nurses[n] = Nurse.makeNurse(rng.nextInt(1,4));
+        }
     }
-    public void run(boolean manual_step){
-        if(manual_step == true){
-            int block_num = 1;
-            System.out.println("You are using manual mode!");
-            while(CurrentTime<=EndTime) {
-                System.out.println("Block " +block_num+ " Start");
-                hospital.update(CurrentTime);
-                CurrentTime += TimeIncrement;
-                boolean n = next_step();
-                if(n == true){
-                    block_num +=1;
-                }
-            }
-        }else{
-            int block_num = 1;
-            System.out.println("You are using automatic mode!");
-            while(CurrentTime<=EndTime){
-                System.out.println("Block " +block_num+ " Start");
-                hospital.update(CurrentTime);
-                CurrentTime += TimeIncrement;
-                block_num +=1;
+
+    public void print_test() {
+        for (int i = 0; i < patients.length; i++) {
+            Patient p = patients[i];
+            if (patients[i] != null) {
+                System.out.println("Name: " +p.getPatient_firstname()+ " " +p.getPatient_lastname()+ " Sex: " +p.getPatient_sex()+ " UUID: " +p.getPatient_uuid()+ " Date of Birth: " +p.getPatient_date_of_birth());
             }
         }
     }
 
-    public static boolean next_step() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Type 'y' to go to next block (y/n)? ");
-        String input_string = sc.next();
-        return(input_string.equals("y"));
+    public void update(int cur_time){
+        for(int i = 0; i<patients.length; i++){
+            patients[i].update(cur_time);
+            nurses[i].update_nurse(cur_time);
+        }
     }
 }
